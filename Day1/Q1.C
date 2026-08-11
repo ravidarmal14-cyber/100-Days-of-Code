a786120563ab46f645eff36bfbf00d1f32ab46f2
Ravi Darmal <308974696+ravidarmal14-cyber@users.noreply.github.com>
2026-08-11 23:37:47 +0530

    first commit

---
+++ Day1/Q1.C
@@ -0,0 +1,28 @@
+/*Q1: Write a program to input two numbers and display their sum.
+
+
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+# include<stdio.h>
+
+int main () {
+    int a;
+    int b;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    printf("The sum is: %d", a + b);
+    
+}
