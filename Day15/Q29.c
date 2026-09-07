cda97a4de2d1e7c8a2ed286d07e93815b853925c
ravidarmal14-cyber <308974696+ravidarmal14-cyber@users.noreply.github.com>
2026-09-07 23:01:59 +0530

    Add comments for factorial program
    Added comments for program functionality and sample test cases.

--- Day15/Q29.c
+++ Day15/Q29.c
@@ -1,3 +1,17 @@
+/*Q29: Write a program to calculate the factorial of a number.
+
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+120
+
+Input 2:
+3
+Output 2:
+6
+
+*/
 #include <stdio.h>
 
 int main() {
